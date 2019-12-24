f20055d215c230b5c294282f8862d515f2e37787
zgang11 <45088871+zgang11@users.noreply.github.com>
2019-12-24 18:08:39 +0800

    12.24

--- text-cluster-master/VENV/system_webfront/article/urls.py
+++ text-cluster-master/VENV/system_webfront/article/urls.py
@@ -8,5 +8,5 @@ app_name = 'article'
 urlpatterns = [
     # 目前还没有urls
     path('article-list/', views.article_list, name='article_list'),
-    #path('article-detail/<int:id>/', views.article_detail, name='article_detail')
+    path('article-detail/<int:id>/', views.article_detail, name='article_detail')
 ]
