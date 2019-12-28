5574cb8cb72048b5b78399a104285d78d909e804
zgang11 <45088871+zgang11@users.noreply.github.com>
2019-12-28 12:57:18 +0800

    12.57

--- text-cluster-master/VENV/system_webfront/article/admin.py
+++ text-cluster-master/VENV/system_webfront/article/admin.py
@@ -1,9 +1,8 @@
-from django.contrib import admin
-
 # Register your models here.
 from django.contrib import admin
 
-from .models import ArticlePost
+from .models import ArticlePost, DailyData
 
 admin.site.register(ArticlePost)
+admin.site.register(DailyData)
 
